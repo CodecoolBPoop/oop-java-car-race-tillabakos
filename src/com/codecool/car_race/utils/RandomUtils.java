b8ed74007233c79e910cc9d8184b3fa79b182f94
tillabakos <bakos.at@gmail.com>
2018-09-05 15:40:20 +0200

    Removed getters setters from Vehicle class - replaced with method that is used by all child classes.

--- src/com/codecool/car_race/utils/RandomUtils.java
+++ src/com/codecool/car_race/utils/RandomUtils.java
@@ -6,8 +6,9 @@ public class RandomUtils {
 
   private static final Random random = new Random();
 
-  public static int nextInt(int upper) {
+  public static int randomIntFromRange(int upper) {
     return random.nextInt(upper);
   }
+
 
 }
